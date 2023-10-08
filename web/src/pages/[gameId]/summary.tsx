132edef9d2632566651bfaae9368c28b0c6f62b8
akhercha <akherchache@pm.me>
2023-10-08 17:13:28 +0200

    feat(game_summary): Refacto

--- web/src/pages/[gameId]/summary.tsx
+++ web/src/pages/[gameId]/summary.tsx
@@ -4,10 +4,10 @@ import Layout, { LeftPanelProps } from "@/components/Layout";
 import { useRouter } from "next/router";
 import { Avatar } from "@/components/avatar/Avatar";
 import { genAvatarFromAddress } from "@/components/avatar/avatars";
-import { useGameEntity } from "@/dojo/entities/useGameEntity";
+import { GameEntity, useGameEntity } from "@/dojo/entities/useGameEntity";
 import { useDojo } from "@/dojo";
 import BorderImage from "@/components/icons/BorderImage";
-import { usePlayerEntity } from "@/dojo/entities/usePlayerEntity";
+import { PlayerEntity, usePlayerEntity } from "@/dojo/entities/usePlayerEntity";
 import { formatCash } from "@/utils/ui";
 import colors from "@/theme/colors";
 
@@ -22,36 +22,48 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-export default function Summary() {
-  return (
-    <Layout CustomLeftPanel={() => <CustomLeftPanel title="Hustler Log" />}>
-      <VStack my="auto" display={["none", "flex"]} gap="20px">
-        <VStack w="full" align="flex-start">
-          <Text>Here we will have the recap!</Text>
-        </VStack>
-      </VStack>
-      <VStack display={["flex", "none"]}>
-        <Text>Here we will have the recap for mobile!</Text>
-      </VStack>
-    </Layout>
-  );
+interface dojoEntitiesProps {
+  playerEntity?: PlayerEntity;
+  gameEntity?: GameEntity;
 }
 
-const CustomLeftPanel: React.FC<LeftPanelProps> = ({
-  title,
-  prefixTitle,
-  ...props
-}) => {
+export default function Summary() {
   const router = useRouter();
-  const { account } = useDojo();
-  const gameId = router.query.gameId as string;
 
+  const gameId = router.query.gameId as string;
+  const { account } = useDojo();
   const { player: playerEntity } = usePlayerEntity({
     gameId,
     address: account?.address,
   });
+  const { game: gameEntity } = useGameEntity({
+    gameId,
+  });
+
   return (
-    <VStack flex={["0", "1"]} my={["none", "auto"]} {...props}>
+    <Layout
+      CustomLeftPanel={() => (
+        <CustomLeftPanel
+          title="Hustler Log"
+          playerEntity={playerEntity}
+          gameEntity={gameEntity}
+        />
+      )}
+    >
+      <PlayerRecap gameEntity={gameEntity} playerEntity={playerEntity} />
+    </Layout>
+  );
+}
+
+const CustomLeftPanel: React.FC<LeftPanelProps & dojoEntitiesProps> = ({
+  title,
+  prefixTitle,
+  playerEntity,
+  gameEntity,
+  ...props
+}) => {
+  return (
+    <VStack mt={[4, 50]} flex={["0", "1"]} {...props}>
       <VStack
         zIndex="1"
         position={playerEntity ? "relative" : "absolute"}
@@ -65,25 +77,23 @@ const CustomLeftPanel: React.FC<LeftPanelProps> = ({
           {title}
         </Heading>
       </VStack>
-      <PlayerStats />
+      <PlayerStats playerEntity={playerEntity} gameEntity={gameEntity} />
     </VStack>
   );
 };
 
-const PlayerStats = () => {
-  const router = useRouter();
+const PlayerStats: React.FC<dojoEntitiesProps> = ({
+  playerEntity,
+  gameEntity,
+}) => {
+  if (!playerEntity || !gameEntity)
+    return <Text my="auto">No account or game found.</Text>;
+
   const { account } = useDojo();
-  const gameId = router.query.gameId as string;
-  const { game: gameEntity } = useGameEntity({
-    gameId,
-  });
-  const { player: playerEntity } = usePlayerEntity({
-    gameId,
-    address: account?.address,
-  });
-  const address = account?.address || "";
-  return playerEntity && gameEntity ? (
-    <HStack h="lg">
+  const address: string = account?.address || "";
+
+  return (
+    <HStack my={["auto", "auto"]} mt={["10", "auto"]}>
       <Card
         p={4}
         paddingBottom={2}
@@ -109,12 +119,12 @@ const PlayerStats = () => {
           border="2px"
           borderColor="neon.900"
         />
-        <GridItem p="6px" colSpan={1} border="1px" borderColor="neon.700">
+        <GridItem p="2" colSpan={1} border="1px" borderColor="neon.700">
           <HStack>
             <Gem /> <Text>{formatCash(playerEntity.cash)}</Text>
           </HStack>
         </GridItem>
-        <GridItem p="6px" colSpan={1} border="1px" borderColor="neon.700">
+        <GridItem p="2" colSpan={1} border="1px" borderColor="neon.700">
           <HStack>
             <Calendar />{" "}
             <Text>
@@ -122,14 +132,28 @@ const PlayerStats = () => {
             </Text>
           </HStack>
         </GridItem>
-        <GridItem p="6px" colSpan={1} border="1px" borderColor="neon.700">
+        <GridItem p="2" colSpan={1} border="1px" borderColor="neon.700">
           <HStack>
             <Heart /> <Text>{playerEntity.health}</Text>
           </HStack>
         </GridItem>
       </Grid>
     </HStack>
-  ) : (
-    <></>
+  );
+};
+
+const PlayerRecap: React.FC<dojoEntitiesProps> = (playerEntity, gameEntity) => {
+  if (!playerEntity || !gameEntity) return <></>;
+  return (
+    <>
+      <VStack mt={[4, 20]} display={["none", "flex"]}>
+        <VStack w="full" align="flex-start">
+          <Text>Here we will have the recap for desktops!</Text>
+        </VStack>
+      </VStack>
+      <VStack display={["flex", "none"]} p="10" paddingTop={"20"}>
+        <Text>Here we will have the recap for mobiles!</Text>
+      </VStack>
+    </>
   );
 };
